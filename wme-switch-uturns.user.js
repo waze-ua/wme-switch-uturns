4f68917442f0352d6fba2b3f2ebfcbf08269ebc8
Anton Shevchuk <antonshevchuk@gmail.com>
2022-08-29 17:04:36 +0300

    Migrate to new libraries

--- wme-switch-uturns.user.js
+++ wme-switch-uturns.user.js
@@ -1,26 +1,31 @@
 // ==UserScript==
 // @name         WME Switch Uturns
-// @version      2022.08.24.001
+// @version      2022.08.28.001
 // @description  Switches U-turns for selected node or segment. Forked and improved "WME Add Uturn from node" script.
 // @author       ixxvivxxi, uranik, turbopirate, AntonShevchuk
-// @include      /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
-// @grant        none
-// @require      https://greasyfork.org/scripts/389117-apihelper/code/APIHelper.js?version=1084940
-// @require      https://greasyfork.org/scripts/389577-apihelperui/code/APIHelperUI.js?version=1082967
 // @namespace    https://github.com/waze-ua/wme-switch-uturns
 // @updateURL    https://github.com/waze-ua/wme-switch-uturns/raw/master/wme-switch-uturns.user.js
 // @downloadURL  https://github.com/waze-ua/wme-switch-uturns/raw/master/wme-switch-uturns.user.js
+// @match        https://www.waze.com/editor*
+// @match        https://www.waze.com/*/editor*
+// @match        https://beta.waze.com/editor*
+// @match        https://beta.waze.com/*/editor*
+// @exclude      https://www.waze.com/user/editor*
+// @exclude      https://beta.waze.com/user/editor*
+// @grant        none
+// @require      https://greasyfork.org/scripts/450160-wme-bootstrap/code/WME-Bootstrap.js?version=1087270
+// @require      https://greasyfork.org/scripts/450221-wme-base/code/WME-Base.js?version=1087271
+// @require      https://greasyfork.org/scripts/450320-wme-ui/code/WME-UI.js?version=1087272
 // ==/UserScript==
 
-/* jshint esversion: 6 */
+/* jshint esversion: 8 */
 /* global require */
 /* global $ */
 /* global W */
-/* global OL */
 /* global I18n */
-/* global APIHelper */
+/* global WME, WMEBase, WMEUI, WMEUIHelper, WMEUIShortcut */
 
-(function ($) {
+(function () {
   'use strict'
 
   // Script name, uses as unique index
@@ -54,153 +59,188 @@
     }
   }
 
-  APIHelper.bootstrap()
-  APIHelper.addTranslation(NAME, TRANSLATION)
-  APIHelper.addStyle(
-    '#node-edit-general button { margin-bottom: 2px }'
-  )
+  const STYLE = '#node-edit-general button { margin-bottom: 2px }' +
+    'p.info-bar { border-top: 1px solid #ccc; color: #777; font-size: x-small; margin-top: 15px; padding-top: 10px; text-align: center; }'
 
-  $(document)
-    .on('init.apihelper', ready)
-    .on('node.apihelper', createNodeUI)
+  WMEUI.addTranslation(NAME, TRANSLATION)
+  WMEUI.addStyle(STYLE)
 
-  let sl, label, div, text, allow, disallow
+  class UTurns extends WMEBase {
+    constructor (name) {
+      super(name)
+    }
+
+    /**
+     * Handler for `node.wme` event
+     * @param {jQuery.Event} event
+     * @param {HTMLElement} element
+     * @param {W.model} model
+     * @return {Null}
+     */
+    onNode (event, element, model) {
+      if (model.getSegmentIds().length < 2) {
+        return
+      }
+      this.createPanel(element)
+      this.updateNodeUI()
+    }
+
+    /**
+     * Added controls
+     * @param {HTMLElement} element
+     */
+    createPanel (element) {
+      // Separator line
+      let sl = document.createElement('div')
+      sl.className = 'separator-line'
+      sl.style.marginBottom = '16px'
+      // Label
+      let label = document.createElement('label')
+      label.innerHTML = I18n.t(NAME).title
+      label.className = 'control-label'
+      // Controls div
+      let div = document.createElement('div')
+      div.className = 'controls'
+      // Text
+      this.text = document.createElement('p')
+      div.append(this.text)
+      // Allow button
+      // <wz-button color="shadowed" class="disallow-connections">Заборонити всі повороти</wz-button>
+      this.allow = document.createElement('wz-button')
+      this.allow.color = 'shadowed'
+      this.allow.innerHTML = I18n.t(NAME).allow_uturns
+      this.allow.onclick = () => this.switchNodeUturn(1)
+      div.append(this.allow)
+      // Separator space
+      div.append(document.createElement('p'))
+      // Disallow button
+      this.disallow = document.createElement('wz-button')
+      this.disallow.color = 'shadowed'
+      this.disallow.innerHTML = I18n.t(NAME).disallow_uturns
+      this.disallow.onclick = () => this.switchNodeUturn(0)
+      div.append(this.disallow)
+      // Info bar
+      let info = document.createElement('p')
+      info.className = 'info-bar'
+      info.innerHTML = '<a href="' + GM_info.scriptUpdateURL + '">' + GM_info.script.name + '</a> ' + GM_info.script.version
+      div.append(info)
+
+      element.append(sl)
+      element.append(label)
+      element.append(div)
+    }
+
+    /**
+     * Updated buttons status and counters
+     */
+    updateNodeUI () {
+      let node = WME.getSelectedNode()
+      if (!node) {
+        return
+      }
+
+      let counter = this.countNodeUturns(node)
+
+      // Change display properties of the buttons
+      this.allow.style.display = counter.disallowed ? 'inline-block' : 'none'
+      this.disallow.style.display = counter.allowed ? 'inline-block' : 'none'
+
+      // Change text
+      this.text.innerHTML =
+        I18n.t(NAME).allowed + ': ' + counter.allowed + '<br/>' +
+        I18n.t(NAME).disallowed + ': ' + counter.disallowed
+    }
+
+    /**
+     * @param node
+     * @return {{allowed: number, disallowed: number}}
+     */
+    countNodeUturns (node) {
+      let counter = {
+        allowed: 0,
+        disallowed: 0
+      }
+
+      let segmenstIds = node.getSegmentIds()
+
+      for (let i = 0; i < segmenstIds.length; i++) {
+        let segment = W.model.segments.getObjectById(segmenstIds[i])
+        if (!segment) {
+          continue
+        }
+        if (segment.isTurnAllowed(segment, node)) {
+          counter.allowed++
+        } else {
+          counter.disallowed++
+        }
+      }
+      return counter
+    }
+
+    /**
+     * Handler for selected node
+     * @param status
+     */
+    switchNodeUturn (status) {
+      let node = WME.getSelectedNode()
+      if (!node) {
+        return
+      }
+      let segmenstIds = node.getSegmentIds()
+      if (segmenstIds.length < 2) {
+        return
+      }
+      for (let i = 0; i < segmenstIds.length; i++) {
+        let segment = W.model.segments.getObjectById(segmenstIds[i])
+        let turn = W.model.getTurnGraph().getTurnThroughNode(node, segment, segment)
+        W.model.actionManager.add(
+          new WazeActionSetTurn(
+            W.model.getTurnGraph(),
+            turn.withTurnData(turn.getTurnData().withState(status)))
+        )
+      }
+      this.updateNodeUI()
+    }
+
+    /**
+     * Handler for selected segments
+     * @param direction
+     */
+    switchSegmentUturn (direction = 'A') {
+      let segments = WME.getSelectedSegments()
+      for (let i = 0, total = segments.length; i < total; i++) {
+        let segment = segments[i]
+        let node = (direction === 'A') ? segment.getFromNode() : segment.getToNode()
+        let status = segment.isTurnAllowed(segment, node) ? 0 : 1
+        let turn = W.model.getTurnGraph().getTurnThroughNode(node, segment, segment)
+        W.model.actionManager.add(
+          new WazeActionSetTurn(
+            W.model.getTurnGraph(),
+            turn.withTurnData(turn.getTurnData().withState(status)) // enable it
+          )
+        )
+      }
+    }
+  }
 
   let WazeActionSetTurn
 
-  function ready () {
-    // Require Waze components
-    WazeActionSetTurn = require('Waze/Model/Graph/Actions/SetTurn')
-    // Separator line
-    sl = document.createElement('div')
-    sl.className = 'separator-line'
-    sl.style.marginBottom = '16px'
-    // Label
-    label = document.createElement('label')
-    label.innerHTML = I18n.t(NAME).title
-    label.className = 'control-label'
-    // Controls div
-    div = document.createElement('div')
-    div.className = 'controls'
-    // Text
-    text = document.createElement('p')
-    div.append(text)
-    // Allow button
-    allow = document.createElement('wz-button')
-    allow.color = 'shadowed'
-    allow.innerHTML = I18n.t(NAME).allow_uturns
-    allow.onclick = () => switchNodeUturn(1)
-    div.append(allow)
-    // Disallow button
-    disallow = document.createElement('wz-button')
-    disallow.color = 'shadowed'
-    disallow.innerHTML = I18n.t(NAME).disallow_uturns
-    disallow.onclick = () => switchNodeUturn(0)
-    div.append(disallow)
+  $(document)
+    .on('bootstrap.wme', () => {
+      // Require Waze components
+      WazeActionSetTurn = require('Waze/Model/Graph/Actions/SetTurn')
 
-    // Hotkeys for node manipulation
-    new APIHelperUIShortcut(NAME + '-node-allow', I18n.t(NAME).allow_uturns, NAME, I18n.t(NAME).title, 'A+A', () => switchNodeUturn(1), null).add()
-    new APIHelperUIShortcut(NAME + '-node-disallow', I18n.t(NAME).disallow_uturns, NAME, I18n.t(NAME).title, 'A+S', () => switchNodeUturn(0), null).add()
-    // Hotkeys for segment manipulation
-    new APIHelperUIShortcut(NAME + '-segment-a', I18n.t(NAME).switch_uturn + ' A', NAME, I18n.t(NAME).title, 'A+Q', () => switchSegmentUturn('A'), null).add()
-    new APIHelperUIShortcut(NAME + '-segment-b', I18n.t(NAME).switch_uturn + ' B', NAME, I18n.t(NAME).title, 'A+W', () => switchSegmentUturn('B'), null).add()
-    // Update count of UTurns on events
-    W.model.actionManager.events.register('afterundoaction', null, updateNodeUI)
-    W.model.actionManager.events.register('afterclearactions', null, updateNodeUI)
-    W.model.actionManager.events.register('afteraction', null, updateNodeUI)
-  }
+      let UTurnsInstance = new UTurns(NAME)
 
-  function createNodeUI (ev, element) {
-    let node = APIHelper.getSelectedNode()
-    if (!node) {
-      return
-    }
-    if (node.getSegmentIds().length < 2) {
-      return
-    }
-    element.append(sl)
-    element.append(label)
-    element.append(div)
-    // Refresh
-    updateNodeUI()
-  }
-
-  function updateNodeUI () {
-    let node = APIHelper.getSelectedNode()
-    if (!node) {
-      return
-    }
-    let counter = countNodeUturns(node)
-
-    // Change display properties of the buttons
-    allow.style.display = counter.disallowed ? 'inline-block' : 'none'
-    disallow.style.display = counter.allowed ? 'inline-block' : 'none'
-
-    // Change text
-    text.innerHTML =
-      I18n.t(NAME).allowed + ': ' + counter.allowed + '<br/>' +
-      I18n.t(NAME).disallowed + ': ' + counter.disallowed
-
-  }
-
-  function countNodeUturns (node) {
-    let counter = {
-      allowed: 0,
-      disallowed: 0
-    }
-
-    let segmenstIds = node.getSegmentIds()
-
-    for (let i = 0; i < segmenstIds.length; i++) {
-      let segment = W.model.segments.getObjectById(segmenstIds[i])
-      if (!segment) {
-        continue
-      }
-      if (segment.isTurnAllowed(segment, node)) {
-        counter.allowed++
-      } else {
-        counter.disallowed++
-      }
-    }
-    return counter
-  }
-
-  // Handler for selected node
-  function switchNodeUturn (status) {
-    let node = APIHelper.getSelectedNode()
-    if (!node) {
-      return
-    }
-    let segmenstIds = node.getSegmentIds()
-    if (segmenstIds.length < 2) {
-      return
-    }
-    for (let i = 0; i < segmenstIds.length; i++) {
-      let segment = W.model.segments.getObjectById(segmenstIds[i])
-      let turn = W.model.getTurnGraph().getTurnThroughNode(node, segment, segment)
-      W.model.actionManager.add(
-        new WazeActionSetTurn(
-          W.model.getTurnGraph(),
-          turn.withTurnData(turn.getTurnData().withState(status)))
-      )
-    }
-    updateNodeUI()
-  }
-
-  // Handler for selected segments
-  function switchSegmentUturn (direction = 'A') {
-    let segments = APIHelper.getSelectedSegments()
-    for (let i = 0, total = segments.length; i < total; i++) {
-      let segment = segments[i]
-      let node = (direction === 'A') ? segment.getFromNode() : segment.getToNode()
-      let status = segment.isTurnAllowed(segment, node) ? 0 : 1
-      let turn = W.model.getTurnGraph().getTurnThroughNode(node, segment, segment)
-      W.model.actionManager.add(
-        new WazeActionSetTurn(
-          W.model.getTurnGraph(),
-          turn.withTurnData(turn.getTurnData().withState(status)) // enable it
-        )
-      )
-    }
-  }
-})(window.jQuery)
+      // Hotkeys for node manipulation
+      new WMEUIShortcut(NAME + '-node-allow', I18n.t(NAME).allow_uturns, NAME, I18n.t(NAME).title, 'A+A', () => UTurnsInstance.switchNodeUturn(1))
+      new WMEUIShortcut(NAME + '-node-disallow', I18n.t(NAME).disallow_uturns, NAME, I18n.t(NAME).title, 'A+S', () => UTurnsInstance.switchNodeUturn(0))
+      // Hotkeys for segment manipulation
+      new WMEUIShortcut(NAME + '-segment-a', I18n.t(NAME).switch_uturn + ' A', NAME, I18n.t(NAME).title, 'A+Q', () => UTurnsInstance.switchSegmentUturn('A'))
+      new WMEUIShortcut(NAME + '-segment-b', I18n.t(NAME).switch_uturn + ' B', NAME, I18n.t(NAME).title, 'A+W', () => UTurnsInstance.switchSegmentUturn('B'))
+      // Update count of UTurns on events
+      W.model.actionManager.events.register('afterundoaction', null, () => UTurnsInstance.updateNodeUI())
+      W.model.actionManager.events.register('afterclearactions', null, () => UTurnsInstance.updateNodeUI())
+      W.model.actionManager.events.register('afteraction', null, () => UTurnsInstance.updateNodeUI())
+    })
+})()
