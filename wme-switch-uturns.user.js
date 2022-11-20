d3f411749e4accfe3e5df4404178cfcafd5bced7
Anton Shevchuk <antonshevchuk@gmail.com>
2022-11-20 22:15:37 +0200

    Increase version

--- wme-switch-uturns.user.js
+++ wme-switch-uturns.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WME Switch Uturns
-// @version      2022.10.27.001
+// @version      2022.11.20.001
 // @description  Switches U-turns for selected node or segment. Forked and improved "WME Add Uturn from node" script.
 // @author       ixxvivxxi, uranik, turbopirate, AntonShevchuk
 // @namespace    https://github.com/waze-ua/wme-switch-uturns
