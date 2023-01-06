920b6501a5ba3bc2af5924b6d401aee173bdce41
Anton Shevchuk <antonshevchuk@gmail.com>
2023-01-06 17:35:50 +0100

    Merged

--- wme-switch-uturns.user.js
+++ wme-switch-uturns.user.js
@@ -3,9 +3,9 @@
 // @version      2023.01.06.001
 // @description  Switches U-turns for selected node or segment. Forked and improved "WME Add Uturn from node" script.
 // @author       ixxvivxxi, uranik, turbopirate, AntonShevchuk
-// @namespace    https://greasyfork.org/ru/users/160654-waze-ukraine
-// @updateURL    https://greasyfork.org/ru/scripts/457553-wme-switch-uturns
-// @downloadURL  https://greasyfork.org/ru/scripts/457553-wme-switch-uturns
+// @namespace    https://greasyfork.org/users/160654-waze-ukraine
+// @updateURL    https://greasyfork.org/scripts/457553-wme-switch-uturns
+// @downloadURL  https://greasyfork.org/scripts/457553-wme-switch-uturns
 // @match        https://*.waze.com/editor*
 // @match        https://*.waze.com/*/editor*
 // @exclude      https://*.waze.com/user/editor*
